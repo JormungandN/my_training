a9b26404a8a94a882b1222759527b657b3d96bfd
JormungandN <hatbe426@gmail.com>
2024-11-19 13:54:32 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+student_and_grads = {'Johnny':[5,3,3,5,4],'Bilbo':[2,2,2,3],'Steve':[4,5,5,2],'Khendrik':[4,4,3], 'Aaron':[5,5,5,4,5]}
+johnny = [5,3,3,5,4]
+j = sum(johnny)//len(johnny)
+print('Cредний бал johnny:',j)
+Bilbo = [2,2,2,3]
+b = sum(Bilbo)//len(Bilbo)
+print('Средний бал Bilbo:',b)
+Steve = [4,5,5,2]
+s = sum(Steve)//len(Steve)
+print('Средний бал Steve:',s)
+Khendrik = [4,4,3]
+k = sum(Khendrik)//len(Khendrik)
+print('Средний бал Steve:',k)
+Aaron = [5,5,5,4,5]
+a = sum(Aaron)//len(Aaron)
+print('Средний бал Steve:',a)
+print(student_and_grads)
